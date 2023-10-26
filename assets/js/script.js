91cb98daa13943bfa28659c894c8c0c0c63b9f36
Menna-Shamloul <>
2023-10-26 20:30:01 +0000

    add winning conditions and add popup meassge

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+let btn = document.querySelectorAll(".button-opt");
+let popup = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let message = document.getElementById("message");
+
+let winning = [
+    [0,1,2],
+    [0,3,6],
+    [2,5,8],
+    [6,7,8],
+    [3,4,5],
+    [1,4,7],
+    [0,4,8],
+    [2,4,6],
+];
+
+let xTurn = true;
+let count = 0;
+
+const disableButtons = () =>{
+    btn.forEach((element) => (element.disabled = true));
+    popup.classList.remove("hide");
+};
+
+const enableButtons = () =>{
+    btn.forEach((element) =>{
+        element.innerText = "";
+        element.disabled = false;
+    });
+    popup.classList.add("hide");
+};
